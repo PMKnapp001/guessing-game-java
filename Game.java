4aca35bfd591f777b6707855e2d8ec56806beac1
PMKnapp001 <pmknapp001@outlook.com>
2023-01-20 13:00:53 -0500

    asssignment started, need to finish coding from psuedocode

--- Game.java
+++ Game.java
@@ -1,6 +1,31 @@
 /** A number guessing game. */
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Game {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    System.out.println("Welcome to the Guessing Game!");
+
+    Scanner input = new Scanner(System.in);
+    Random randomNum = new Random();
+    int randomNumber = randomNum.nextInt(1, 101);
+    int guessCount = 0;
+  
+    System.out.println("Enter your name: ");
+    String name = input.nextLine();
+    System.out.println(name + ", I'm thinking of a number between 1 and 100.");
+
+    //use while loop to play until correct guess is made
+    System.out.println("Try to guess the number.");
+    System.out.println("Enter your guess: ");
+    //get user input (nextInt)
+    //increment guessCount per loop
+    //use if, elseif, else to dictate logic flow for hints
+    //if too high
+    //elsif too low
+    //else right guess
+
+    //print winning message wih guess count
   }
 }
